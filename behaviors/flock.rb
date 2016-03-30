022b215f11bea69cf151b7c3f7af30d55b6bfb6b
Milo Trujillo <milo.trujillo@daylightingsociety.org>
2016-03-30 00:41:12 -0400

    Added more example behaviors

---
+++ behaviors/flock.rb
@@ -0,0 +1,56 @@
+#!/usr/bin/env jruby
+
+require_relative "../util"
+require_relative "../configuration"
+
+=begin
+	EXAMPLE BUG BEHAVIOR
+
+	Followers will follow their closest neighbor unless they get 
+	too close or far. They will seek out or run from bugs if needed.
+=end
+
+def bugStep(bug, bugs)
+	# Some variables we'll need
+	new_x = -1
+	new_y = -1
+	x = bug.x
+	y = bug.y
+	lastAction = nil
+
+	(closest, closestDistance) = findClosest(x, y, bugs)
+	#puts "Got closest distance #{closestDistance}"
+	if( closestDistance > BugMaxDistance ) # Go towards friends if lonely
+		(new_x, new_y) = stepTowards(x, y, closest.x, closest.y, BugStep)
+		x = new_x if( new_x > 0 && new_x < ScreenWidth )
+		y = new_y if( new_y > 0 && new_y < ScreenHeight )
+		lastAction = "towards"
+	elsif( closestDistance <= BugMinDistance ) # Move away if overpopulated
+		(new_x, new_y) = stepAway(x, y, closest.x, closest.y, BugStep)
+		x = new_x if( new_x > 0 && new_x < ScreenWidth )
+		y = new_y if( new_y > 0 && new_y < ScreenHeight )
+		lastAction = "away"
+	else # Go same way as our friend otherwise
+		if( rand() < 0.995 )
+			(new_x, new_y) = stepDirection(x, y, closest.direction, BugStep)
+		else
+			(new_x, new_y) = stepRand(x, y, BugStep)
+		end
+		x = new_x if( new_x > 0 && new_x < ScreenWidth )
+		y = new_y if( new_y > 0 && new_y < ScreenHeight )
+		lastAction = "rand"
+	end
+
+	# Try to bounce off screen edges
+	if( x != new_x )
+		(new_x, _) = stepRand(x, y, BugStep * 3.0)
+		x = new_x if( new_x > 0 && new_x < ScreenWidth )
+	end
+	if( y != new_y )
+		(_, new_y) = stepRand(x, y, BugStep * 3.0)
+		#puts "Bouncing X #{y} to #{new_y}"
+		y = new_y if( new_y > 0 && new_y < ScreenHeight )
+	end
+
+	return [x, y, lastAction]
+end
