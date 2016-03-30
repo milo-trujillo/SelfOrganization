152ec435d2ef62dd5e9b4cb44887e5e3d55df2c6
Milo Trujillo <milo.trujillo@daylightingsociety.org>
2016-03-29 20:03:23 -0400

    Added handy launch script

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# NOTE: To run you will need to have Processing 3 and JRubyArt installed
+# Instructions can be found here: https://github.com/ruby-processing/JRubyArt
+# But be warned! They may not be for the faint of heart! Or at least they
+# require a lot of mucking around on the command line.
+
+k9 run main.rb
